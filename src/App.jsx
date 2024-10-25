2a9e76307bd5e68ec50cefb41d70797a6d6ff389
WGreenwichChaca <walter.greenwich@gmail.com>
2024-10-25 11:22:37 -0500

    Añadiendo .gitignore y otros archivos

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./style";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
+    </div>
+  </div>
+);
+
+export default App;
